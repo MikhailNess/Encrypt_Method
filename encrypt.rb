a3187860598507c4444c3ce73b67797507fcc223
mikhail <miha_nesss@mail.ru>
2022-05-17 05:50:00 +0500

    First version

---
+++ encrypt.rb
@@ -0,0 +1,17 @@
+#шифрование
+require 'digest'
+puts "Введите слово или фразу для шифрования:"
+string = STDIN.gets.chomp
+
+puts "Каким способом зашифровать? "
+puts "1. MD5 "
+puts "2. SHA1 "
+
+encrypt_method = STDIN.gets.to_i
+
+case encrypt_method
+when 1
+  puts Digest::MD5.hexdigest(string)
+when 2
+  puts Digest::SHA1.hexdigest(string)
+end
